39e7920202b676308dded4bd508f55ed3ad49779
ahmed eladly <ahmed.eladly2010@gmail.com>
2023-02-17 18:52:29 -0800

    edit 6-size.c project

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,14 +3,14 @@
 /**
  * main - Entry point
  * printf : prints size of characters
- * Retuen :0
+ * return : 0
  */
 int main(void)
 {
-	printf("size of a char: %zu bytes(s)\n",sizeof(char));
-	printf("size of a int: %zu bytes(s)\n",sizeof(int));
-	printf("size of a long int: %zu bytes(s)\n",sizeof(long int));
-	printf("size of a long long int: %zu bytes(s)\n",sizeof(long long int));
-	printf("size of a float: %zu bytes(s)\n",sizeof(float));
+	printf("size of a char: %zu bytes(s)\n", sizeof(char));
+	printf("size of a int: %zu bytes(s)\n", sizeof(int));
+	printf("size of a long int: %zu bytes(s)\n", sizeof(long int));
+	printf("size of a long long int: %zu bytes(s)\n", sizeof(long long int));
+	printf("size of a float: %zu bytes(s)\n", sizeof(float));
 	return (0);
 }
