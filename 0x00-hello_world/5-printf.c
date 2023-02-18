e52a74fd5aceb1dede10ae1d39d5f31f3fe5202d
ahmed eladly <ahmed.eladly2010@gmail.com>
2023-02-17 18:28:02 -0800

    editwq 5-printf.c project

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,8 @@
  * Print f : prints with proper grammar, but the outcome is a piece of art, followed by a new line.
  * return : 0
 */
-main(void);
+
+int main(void)
 {
         printf("with proper grammar, but the outcome is a piece of art,\n");
         retrun (0);
