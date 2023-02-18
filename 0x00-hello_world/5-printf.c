712a9376997fb27ded5f531ee970b9d1a3847766
ahmed eladly <ahmed.eladly2010@gmail.com>
2023-02-17 18:36:42 -0800

    editwq 5-printf.c project

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point 
- * Print f : prints with proper grammar, but the outcome is a piece of art, followed by a new line.
+ * Printf : prints with proper grammar, but the outcome is a piece of art, followed by a new line.
  * return : 0
 */
 
