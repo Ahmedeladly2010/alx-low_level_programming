3130ecd6de5c5ea363f330268354a8585e492178
ahmed eladly <ahmed.eladly2010@gmail.com>
2023-02-18 14:10:15 -0800

    add 2-print_alphabet.c project

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
-/** main - Entry point
+/*
+ * main - Entry point
  * Description: prints the alphabet in lowercase\n
  * return : 0
  */
 int main(void)
 {
 	char letter = 'a';
-	while (letter <= 'z'){
+
+	while (letter <= 'z')
+	{
 		putchar(letter);
 		letter++;
 	}
 	putchar('\n');
 	return (0);
+
 }
