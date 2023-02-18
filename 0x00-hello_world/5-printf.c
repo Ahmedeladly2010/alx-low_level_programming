16944d71baece3d512cbfa7029a6549e38964748
ahmed eladly <ahmed.eladly2010@gmail.com>
2023-02-17 18:23:28 -0800

    add 5-printf.c project

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ * Print f : prints with proper grammar, but the outcome is a piece of art, followed by a new line.
+ * return : 0
+*/
+main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        retrun (0);
+}
