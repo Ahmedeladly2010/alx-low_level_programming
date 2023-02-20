5c475eb4428d9eef247e01999ba50c69c546fa4e
ahmed eladly <ahmed.eladly2010@gmail.com>
2023-02-20 10:42:37 -0800

    edit #include <stdio.h> project

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
- * Print f : prints with proper grammar, but the outcome is a piece of art, followed by a new line.
- * return : 0
-*/
+ * main - Entry point
+ * printf: prints a string followed by a new line.
+ * Return: 0
+ */
 
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        retrun (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
