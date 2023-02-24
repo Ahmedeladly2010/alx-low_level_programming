092b413a14fdd97b019523033efc9ad67729fc68
ahmed eladly <ahmed.eladly2010@gmail.com>
2023-02-23 17:12:53 -0800

    add readme and project 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if character is upper case 
+ * c : variable text
+ * return: always 0
+ */
+int _isuppe(int c)
+{
+	if (c>= 'A' && c<= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
