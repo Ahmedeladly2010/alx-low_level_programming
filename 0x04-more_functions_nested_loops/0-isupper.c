ad6b5ac4dca727c8adc5a76b80ca1e43012da0c9
ahmed eladly <ahmed.eladly2010@gmail.com>
2023-02-24 09:14:47 -0800

    add main.h and 0-isupper.c project

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * _isupper - checks if a character is uppercase 
- * @c: variable text
- * Return: always 0
+ * _isupper - checks is a character is uppercase
+ * @c: carrier integer variable
+ * Return: 1 if true, 0 if false
  */
+
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
